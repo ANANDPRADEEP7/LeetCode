0fdfbdd43f8e1de6c821ff9bcc727278205bd3ff
ANANDPRADEEP <anandpradeep181@gmail.com>
2025-09-24 23:14:43 +0530

    Time: 3 ms (9.12%), Space: 56.2 MB (5.12%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    const n=nums.length;
+    for(let i=0;i<n;i++){
+        for(let j=0;j<n-i-1;j++){
+            if(nums[j]>nums[j+1]){
+                [nums[j],nums[j+1]]=[nums[j+1],nums[j]]
+
+            }
+        }
+    }
+    return nums
+};
