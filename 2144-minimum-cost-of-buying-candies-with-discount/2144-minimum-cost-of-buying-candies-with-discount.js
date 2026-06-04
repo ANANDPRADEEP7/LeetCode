14709e8f760579d9cdf1f3746750c6fd5ca42411
ANANDPRADEEP <anandpradeep181@gmail.com>
2026-06-04 13:05:22 +0530

    Time: 3 ms (25.64%), Space: 54.4 MB (54.56%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minimumCost = function(cost) {
+    let sum=0;
+    cost=cost.sort((b,a)=>a-b)
+    for(let i=0;i<cost.length;i++){
+        if(i%3!==2){
+            sum+=cost[i]
+        }
+    }
+    return sum
+};
